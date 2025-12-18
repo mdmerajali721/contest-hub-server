86d8a4aaf3fe728e3ea032ad63bb3e98b5506eca
Md. Meraj Ali <m2070900m@gmail.com>
2025-12-18 12:32:01 +0600

    server pakage download & setup index

--- firebase-admin-keyConverter.js
+++
@@ -1,5 +0,0 @@
-const fs = require("fs");
-const jsonData = fs.readFileSync("./fb-admin-key.json");
-
-const base64String = Buffer.from(jsonData, "utf-8").toString("base64");
-console.log(base64String);

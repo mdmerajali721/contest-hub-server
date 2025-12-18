20a70b2b13f8eeb1b7534f25fe7857f10bed0c05
Md. Meraj Ali <m2070900m@gmail.com>
2025-12-18 12:46:13 +0600

    add firebase admin sdk for token verification middleware

--- index.js
+++ index.js
@@ -3,13 +3,40 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const cors = require("cors");
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const admin = require("firebase-admin");
 
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
+  "utf8"
+);
+const serviceAccount = JSON.parse(decoded);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+// middleware
+const verifyFBToken = async (req, res, next) => {
+  const token = req.headers?.authorization;
+
+  if (!token) {
+    return res.status(401).send({ message: "unauthorize access" });
+  }
+
+  try {
+    const idToken = token.split(" ")[1];
+    const decoded = await admin.auth().verifyIdToken(idToken);
+    req.decoded_email = decoded.email;
+    next();
+  } catch (error) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+};
 
 const client = new MongoClient(process.env.DB_URI, {
   serverApi: {
@@ -18,6 +45,28 @@ const client = new MongoClient(process.env.DB_URI, {
     deprecationErrors: true,
   },
 });
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("contesthubDB");
+    const usersCollection = database.collection("users");
+    const contestsCollection = database.collection("contests");
+    const paymentsCollection = database.collection("payments");
+
+    // middleware more with database access
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded_email;
+      const query = { email };
+
+      const user = await usersCollection.findOne(query);
+      if (!user || user.role !== "admin") {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+
+      next();
+    };
+  
   
 app.get("/", (req, res) => {
   res.send("Contest Hub Server is Running");

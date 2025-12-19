360eaf8c42b0511ec99997d835868920f2cac60b
Md. Meraj Ali <m2070900m@gmail.com>
2025-12-19 09:35:54 +0600

    index.js final update

--- index.js
+++ index.js
@@ -604,12 +604,7 @@ async function run() {
         res.status(500).send({ message: "Server Error" });
       }
     });
-
-    // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    // console.log(
-    //   "Pinged your deployment. You successfully connected to MongoDB!"
-    // );
   } finally {
     // await client.close();
   }
